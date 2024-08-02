ec11c73d3bb839d4a447500dba1c2394cf849322
gustavo177 <gtvadf177@hotmail.com>
2024-08-02 18:41:58 -0500

    Creando clase padre abstracta ElementoForm

---
+++ src/org/gustavo/pooclasesabstractas/form/elementos/ElementoForm.java
@@ -0,0 +1,20 @@
+package org.gustavo.pooclasesabstractas.form.elementos;
+
+abstract public class ElementoForm {
+    protected String nombre;
+    protected String valor;
+
+    public ElementoForm() {
+    }
+
+    public ElementoForm(String nombre) {
+        this();
+        this.nombre = nombre;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    abstract public String dibujarHtml();
+}
