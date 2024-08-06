cbdc9c26a9eac861d64a2096379ef9f8c444e043
gustavo177 <gtvadf177@hotmail.com>
2024-08-05 20:31:28 -0500

    Creando su contenido de la clase SelectForm

--- src/org/gustavo/pooclasesabstractas/form/elementos/SelectForm.java
+++ src/org/gustavo/pooclasesabstractas/form/elementos/SelectForm.java
@@ -1,4 +1,45 @@
 package org.gustavo.pooclasesabstractas.form.elementos;
 
-public class SelectForm {
+import org.gustavo.pooclasesabstractas.form.elementos.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectForm  extends  ElementoForm{
+
+    private List<Opcion> opciones;
+
+    public SelectForm(String nombre) {
+        super(nombre);
+        this.opciones = new ArrayList<Opcion>();
+    }
+
+    public SelectForm(String nombre, List<Opcion> opciones) {
+        super(nombre);
+        this.opciones = opciones;
+    }
+    public SelectForm addOpcion(Opcion opcion){
+        this.opciones.add(opcion);
+        return this;
+    }
+    @Override
+    public String dibujarHtml() {
+        StringBuilder sb = new StringBuilder("<select ");
+        sb.append("name='")
+                .append(this.nombre)
+                .append("'>");
+        for (Opcion opcion: this.opciones){
+            sb.append("\n<option value='")
+                    .append(opcion.getValor())
+                    .append("'");
+            if (opcion.isSelected()){
+                sb.append(" selected");
+            }
+            sb.append(">")
+                    .append(opcion.getNombre())
+                    .append("</opcion>");
+        }
+        sb.append("</select>");
+        return sb.toString();
+    }
 }
