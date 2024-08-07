6a9f4b40c5111018f8b964aa2f9c1226bd992a2e
gustavo177 <gtvadf177@hotmail.com>
2024-08-07 16:28:38 -0500

    Clases anónimas

--- src/org/gustavo/pooclasesabstractas/form/elementos/EjemploForm.java
+++ src/org/gustavo/pooclasesabstractas/form/elementos/EjemploForm.java
@@ -2,6 +2,9 @@ package org.gustavo.pooclasesabstractas.form.elementos;
 
 import org.gustavo.pooclasesabstractas.form.elementos.select.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class EjemploForm {
     public static void main(String[] args) {
 
@@ -13,17 +16,48 @@ public class EjemploForm {
         TextareaForm experiencia = new TextareaForm("exp", 5,9);
 
         SelectForm lenguaje = new SelectForm("lenguaje");
-        Opcion java = new Opcion("1", "Java");
-        lenguaje.addOpcion(java);
-        lenguaje.addOpcion(new Opcion("2", "Python"));
-        lenguaje.addOpcion(new Opcion("3", "JavaScript"));
-        lenguaje.addOpcion(new Opcion("4", "TypeScript"));
-        lenguaje.addOpcion(new Opcion("5", "PHP"));
 
+
+
+        lenguaje.addOpcion(new Opcion("1", "Java").setSelected())
+        .addOpcion(new Opcion("2", "Python"))
+        .addOpcion(new Opcion("3", "JavaScript"))
+        .addOpcion( new Opcion("4", "TypeScript"))
+        .addOpcion(new Opcion("5", "PHP"));
+
+        ElementoForm saludar = new ElementoForm() {
+            @Override
+            public String dibujarHtml() {
+                return "<input disabled name='"+this.nombre+"' value=\"" + this.valor + "\">";
+            }
+        };
+        saludar.setValor("Hola que tal este campo está deshabilitado!");
         username.setValor("Tavo.doe");
         password.setValor("123456");
         email.setValor("tavo.doe@correo.com");
         edad.setValor("28");
-        java.setSelected(true);
+        experiencia.setValor("... 5 años de experiencia ...");
+//        java.setSelected(true);
+
+
+        List<ElementoForm> elementos = Arrays.asList(
+                username,
+                password,
+                email,
+                edad,
+                experiencia,
+                lenguaje,
+                saludar
+        );
+
+//        for (ElementoForm e: elementos){
+//            System.out.println(e.dibujarHtml());
+//            System.out.println("<br>");
+//        }
+
+        elementos.forEach(e->{
+            System.out.println(e.dibujarHtml());
+            System.out.println("<br>");
+        });
     }
 }
