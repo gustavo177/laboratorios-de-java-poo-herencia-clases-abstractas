9b62e0e9015686af031314b9b9096e21af70d60f
gustavo177 <gtvadf177@hotmail.com>
2024-08-13 19:59:11 -0500

    Organizando el código

--- src/org/gustavo/pooclasesabstractas/form/validador/NoNuloValidador.java
+++ src/org/gustavo/pooclasesabstractas/form/validador/NoNuloValidador.java
@@ -1,17 +1,14 @@
 package org.gustavo.pooclasesabstractas.form.validador;
-
 public class NoNuloValidador extends  Validador{
     protected String mensaje="el campo no puede ser nulo";
     @Override
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
     }
-
     @Override
     public String getMensaje() {
         return mensaje;
     }
-
     @Override
     public boolean esValido(String valor) {
         return (valor != null);
