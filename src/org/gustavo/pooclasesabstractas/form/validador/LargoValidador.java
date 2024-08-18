e57a708783da4927acee45496f2e528b23c21996
gustavo177 <gtvadf177@hotmail.com>
2024-08-18 14:44:04 -0500

    Creando la clase concreta LargoValidador para validar el min y máx de caracteres

---
+++ src/org/gustavo/pooclasesabstractas/form/validador/LargoValidador.java
@@ -0,0 +1,44 @@
+package org.gustavo.pooclasesabstractas.form.validador;
+
+public class LargoValidador extends Validador{
+    protected String mensaje = "el cmpo debe tener mínimo %d caracteres y máximo %d caracteres";
+    private int min;
+    private int max = Integer.MAX_VALUE;
+
+    public LargoValidador() {
+    }
+
+    public LargoValidador(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje=mensaje;
+
+    }
+
+    @Override
+    public String getMensaje() {
+        return null;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        this.mensaje = String.format(this.mensaje,this.min,this.max);
+        if(valor==null){
+            return true;
+        }
+        int largo = valor.length();
+        return (largo >= min && largo <= max);
+    }
+}
