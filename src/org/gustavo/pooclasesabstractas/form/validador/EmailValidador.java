e73681793d04002d8206cb2bf1452cb87695adc4
gustavo177 <gtvadf177@hotmail.com>
2024-08-20 16:26:04 -0500

    Creando la clase concreta EmailValidador

--- src/org/gustavo/pooclasesabstractas/form/validador/EmailValidador.java
+++ src/org/gustavo/pooclasesabstractas/form/validador/EmailValidador.java
@@ -13,7 +13,6 @@ public class EmailValidador extends Validador{
     public String getMensaje() {
         return mensaje;
     }
-
     @Override
     public boolean esValido(String valor) {
         return valor.matches(EMAIL_REGEX);
