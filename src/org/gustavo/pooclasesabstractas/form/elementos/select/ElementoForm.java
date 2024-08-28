22ac6dca09277a282c51c8cba3fa4bfa3486f3ac
gustavo177 <gtvadf177@hotmail.com>
2024-08-27 19:53:01 -0500

    Ajustando el codigo

--- src/org/gustavo/pooclasesabstractas/form/elementos/select/ElementoForm.java
+++ src/org/gustavo/pooclasesabstractas/form/elementos/select/ElementoForm.java
@@ -1,40 +1,31 @@
 package org.gustavo.pooclasesabstractas.form.elementos.select;
 
 import org.gustavo.pooclasesabstractas.form.validador.Validador;
-
 import java.util.ArrayList;
 import java.util.List;
-
 abstract public class ElementoForm {
     protected String nombre;
     protected String valor;
-
     private List<Validador> validadores;
     private List<String> errores;
-
     public ElementoForm() {
         this.validadores = new ArrayList<>();
         this.errores = new ArrayList<>();
     }
-
     public ElementoForm(String nombre) {
         this();
         this.nombre = nombre;
     }
-
     public ElementoForm addValidador(Validador validador){
         this.validadores.add(validador);
         return this;
     }
-
     public List<String> getErrores() {
         return errores;
     }
-
     public void setValor(String valor) {
         this.valor = valor;
     }
-
     public boolean esValido(){
         for (Validador v: validadores){
             if(!v.esValido(this.valor)){
